bac49c92265928a91927d4355dd56c8a0aa7d5d1
Kavindu Umayangana <161000554+kavinduUmayangana@users.noreply.github.com>
2025-03-18 19:45:34 +0530

    create the db connection

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+$host ="localhost";
+$dbname ="Bookstore";
+$user ="root";
+$pass="";
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+// set the PDO error mode to exception
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if($conn){
+    echo "db connected successfully";
+}else{
+    echo "error in db connection";
+}
+
+
+/*So, if you have to switch your project to use another database,
+ PDO makes the process easy. You only have to change the connection
+ string and a few queries. 
+ With MySQLi,you will need to rewrite the entire code - queries included.*/
